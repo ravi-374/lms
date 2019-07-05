108dd3db3f4593e18aeaf7a591b22f90bbbe3312
Shailesh Ladumor <shaileshmladumor@gmail.com>
2019-07-05 16:06:45 +0530

    fix(footer): minor fix

--- resources/lms/src/components/layout/Footer.js
+++ resources/lms/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <Fragment>
             <span><a href="https://infyom.com">InfyOm</a> &copy; 2019 InfyOmLabs.</span>
-            <span className="ml-auto">Powered by <a href="https://coreui.io/">coreUI</a></span>
+            <span className="ml-auto">Powered by <a href="https://coreui.io/">CoreUI</a></span>
         </Fragment>
     );
 };
