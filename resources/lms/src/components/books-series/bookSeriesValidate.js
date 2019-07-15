86131c77290bfaaa77ae59529f4606d5d323d3dd
Shailesh <shailesh.ladumor@infyom.com>
2019-07-15 05:39:10 +0000

    Revert "Merge branch 'rupesh/user-country' into 'master'"
    This reverts merge request !60

--- resources/lms/src/components/books-series/bookSeriesValidate.js
+++
@@ -1,7 +0,0 @@
-export default formValues => {
-    const errors = {};
-    if (!formValues.title) {
-        errors.title = 'Book series title must be required.';
-    }
-    return errors;
-};
--- resources/lms/src/components/genres/genreValidate.js
+++
@@ -1,7 +0,0 @@
-export default formValues => {
-    const errors = {};
-    if (!formValues.name) {
-        errors.name = 'Genre name must be required.';
-    }
-    return errors;
-};
