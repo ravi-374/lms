f5306e4fd5cd627e08ebc620dad5b9e7f02514b1
Rupesh Halpati <rupesh.halpati@infyom.com>
2019-07-17 11:50:37 +0000

    fix(user/members): fixed user profile broken

--- resources/lms/src/components/members/memberValidate.js
+++ resources/lms/src/components/members/memberValidate.js
@@ -16,6 +16,9 @@ export default formValues => {
     if (!formValues.password) {
         errors.password = 'Password must be required.'
     }
+    if (formValues.password && formValues.password.length < 6) {
+        errors.password = 'Password must be greater than 5 characters.'
+    }
     const phoneExpression = /^\d{10}$/;
     if (formValues.phone && !phoneExpression.test(formValues.phone)) {
         errors.phone = 'Phone no. must be of 10 digit.'
