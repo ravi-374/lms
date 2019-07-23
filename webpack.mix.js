bdb2965fb2b740433a203a3d3eab07cf5e406436
Mitul Golakiya <mtl.golakiya@gmail.com>
2019-07-23 10:04:33 +0530

    chore: #28 removed build warning for postcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,12 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix.options({
+        postCss: [
+            require('autoprefixer'),
+        ],
+});
 mix.setPublicPath('public');
 mix.webpackConfig({
     resolve: {
