587880585478a2901f24bfbec97864018325c8bc
Ankit Kalathiya <Bhavesh>
2019-07-25 17:03:42 +0530

    fix(factories): remove some extra file

--- database/factories/SettingFactory.php
+++ database/factories/SettingFactory.php
@@ -4,7 +4,6 @@
 
 use Faker\Generator as Faker;
 $factory->define(\App\Models\Setting::class, function (Faker $faker) {
-
     return [
         'key'          => $faker->unique()->name,
         'value'        => $faker->word,
