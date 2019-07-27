eb00a52abc796868d5d1d51563dd6c2387e05c44
Ankit Kalathiya <Bhavesh>
2019-07-27 18:51:59 +0530

    refactor(setting): added create and update setting test cases

--- tests/Controllers/Validations/SettingAPIControllerValidationTest.php
+++ tests/Controllers/Validations/SettingAPIControllerValidationTest.php
@@ -4,17 +4,17 @@ namespace Tests\Controllers\Validations;
 
 use App\Models\Setting;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class SettingAPIControllerValidationTest extends TestCase
 {
-    use DatabaseTransactions, WithoutMiddleware;
+    use DatabaseTransactions;
 
     public function setUp(): void
     {
         parent::setUp();
 
+        $this->withoutMiddleware($this->skipMiddleware());
         $this->signInWithDefaultAdminUser();
     }
 
@@ -22,7 +22,7 @@ class SettingAPIControllerValidationTest extends TestCase
     public function test_create_setting_fails_when_key_is_not_passed()
     {
         $this->post('api/b1/settings', ['key' => ''])
-            ->assertSessionHasErrors(['name' => 'The key field is required.']);
+            ->assertSessionHasErrors(['key' => 'The key field is required.']);
     }
 
     /** @test */
@@ -65,4 +65,38 @@ class SettingAPIControllerValidationTest extends TestCase
         $this->put('api/b1/settings/'.$setting->id, ['display_name' => ''])
             ->assertSessionHasErrors(['display_name' => 'The display name field is required.']);
     }
+
+    /** @test */
+    public function it_can_store_publisher()
+    {
+        $response = $this->postJson('api/b1/settings', [
+            'key'          => $this->faker->name,
+            'value'        => $this->faker->word,
+            'display_name' => $this->faker->word,
+        ]);
+
+        $this->assertSuccessMessageResponse($response, 'Setting saved successfully.');
+    }
+
+    /** @test */
+    public function it_can_update_publisher()
+    {
+        /** @var Setting $setting */
+        $setting = factory(Setting::class)->create();
+
+        $key = $this->faker->name;
+        $value = $this->faker->word;
+        $displayName = $this->faker->word;
+        $response = $this->putJson('api/b1/settings/'.$setting->id, [
+            'key'          => $key,
+            'value'        => $value,
+            'display_name' => $displayName,
+        ]);
+
+        $this->assertSuccessMessageResponse($response, 'Publisher updated successfully.');
+        $this->assertEquals($key, $setting->fresh()->key);
+        $this->assertEquals($value, $setting->fresh()->value);
+        $this->assertEquals($displayName, $setting->fresh()->display_name);
+    }
+
 }
