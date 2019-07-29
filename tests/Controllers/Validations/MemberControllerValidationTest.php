03e97c5b5898a018057f34c5a47db7caaa9bdead
Ankit Kalathiya <Bhavesh>
2019-07-29 16:09:54 +0530

    refactor(member): refectored create and update member test case

--- tests/Controllers/Validations/MemberControllerValidationTest.php
+++ tests/Controllers/Validations/MemberControllerValidationTest.php
@@ -76,82 +76,23 @@ class MemberControllerValidationTest extends TestCase
     }
 
     /** @test */
-    public function test_update_member_when_email_is_not_passed()
-    {
-        $ankit = factory(Member::class)->create();
-
-        $this->post('api/b1/members/'.$ankit->id, [
-            'first_name' => 'Ankit',
-            'last_name'  => 'Kalathiya',
-            'email'      => '',
-        ])->assertSessionHasNoErrors();
-    }
-
-    /** @test */
-    public function test_update_member_when_password_is_not_passed()
-    {
-        $ankit = factory(Member::class)->create();
-
-        $this->post('api/b1/members/'.$ankit->id, [
-            'first_name' => 'Ankit',
-            'last_name'  => 'Kalathiya',
-            'password'   => '',
-        ])->assertSessionHasNoErrors();
-    }
-
-    /** @test */
     public function it_can_store_member()
     {
-        $membershipPlan = factory(MembershipPlan::class)->create();
-        $firstName = $this->faker->firstName;
-        $lastName = $this->faker->lastName;
-        $email = $this->faker->unique()->email;
-        $password = $this->faker->password;
-        $membershipPlanId = $membershipPlan->id;
-        $phone = $this->faker->phoneNumber;
-        $response = $this->postJson('api/b1/members', [
-            'first_name'         => $firstName,
-            'last_name'          => $lastName,
-            'email'              => $email,
-            'password'           => $password,
-            'membership_plan_id' => $membershipPlanId,
-            'phone'              => $phone,
-        ]);
+        $fakeMember = factory(Member::class)->make()->toArray();
+        $response = $this->postJson('api/b1/members', $fakeMember);
 
         $this->assertSuccessMessageResponse($response, 'Member saved successfully.');
-        $this->assertNotEmpty(Member::whereFirstName($firstName)->first());
-        $this->assertNotEmpty(Member::whereLastName($lastName)->first());
-        $this->assertNull(Member::wherePassword($password)->first());
-        $this->assertNotEmpty(Member::whereEmail($email)->first());
-        $this->assertNotEmpty(Member::whereMembershipPlanId($membershipPlanId)->first());
-        $this->assertNotEmpty(Member::wherePhone($phone)->first());
+        $this->assertEmpty(Member::whereEmail($fakeMember['email'])->first());
     }
 
     /** @test */
     public function it_can_update_member()
     {
-        /** @var Member $member */
         $member = factory(Member::class)->create();
-        $membershipPlan = factory(MembershipPlan::class)->create();
-        $firstName = $this->faker->firstName;
-        $lastName = $this->faker->lastName;
-        $email = $this->faker->email;
-        $password = $this->faker->password;
-        $phone = $this->faker->phoneNumber;
-        $membershipPlanId = $membershipPlan->id;
-        $response = $this->postJson('api/b1/members/'.$member->id, [
-            'first_name'         => $firstName,
-            'last_name'          => $lastName,
-            'email'              => $email,
-            'password'           => $password,
-            'membership_plan_id' => $membershipPlanId,
-            'phone'              => $phone,
-        ]);
+        $fakeMember = factory(Member::class)->make()->toArray();
+        $response = $this->postJson('api/b1/members/'.$member->id, $fakeMember);
 
         $this->assertSuccessMessageResponse($response, 'Member updated successfully.');
-        $this->assertEquals($firstName, $member->fresh()->first_name);
-        $this->assertEquals($lastName, $member->fresh()->last_name);
-        $this->assertEquals($phone, $member->fresh()->phone);
-        $this->assertEquals($membershipPlanId, $member->fresh()->membership_plan_id);
+        $this->assertNotEquals($fakeMember['email'], $member->fresh()->email);
     }
 }
