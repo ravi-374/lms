85c31f2ccc5579e4b5433dbc61d2b2506066a688
Ankit Kalathiya <Bhavesh>
2019-07-30 10:06:51 +0530

    fix(permission): minor change

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Permission::class, function (Faker $faker) {
 
     return [
-        'name' => $faker->name,
+        'name' => $faker->unique()->name,
     ];
 });
