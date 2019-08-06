c9ac37df738870ba5e9441f9d1d0788083041afe
Farhan <kathawalafarhan@gmail.com>
2019-08-06 13:24:13 +0530

    test(book): test cases for validate book API

--- tests/Controllers/Validations/AuthorAPIControllerValidationTest.php
+++ tests/Controllers/Validations/AuthorAPIControllerValidationTest.php
@@ -14,7 +14,6 @@ class AuthorAPIControllerValidationTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-
         $this->signInWithDefaultAdminUser();
     }
 
@@ -26,11 +25,35 @@ class AuthorAPIControllerValidationTest extends TestCase
     }
 
     /** @test */
+    public function test_create_author_with_valid_input()
+    {
+        $author = $this->prepareAuthor();
+
+        $response = $this->postJson('api/b1/authors', $author);
+
+        $this->assertSuccessDataResponse($response, $author, 'Author saved successfully.');
+    }
+
+    /** @test */
     public function test_update_author_fails_when_first_name_is_not_passed()
     {
         $author = factory(Author::class)->create();
 
         $this->put('api/b1/authors/'.$author->id, ['first_name' => ''])
             ->assertSessionHasErrors(['first_name' => 'The first name field is required.']);
+    }
+
+    /**
+     * @param array $input
+     *
+     * @return array
+     */
+    public function prepareAuthor($input = [])
+    {
+        return array_merge([
+            'first_name'  => $this->faker->firstName,
+            'last_name'   => $this->faker->lastName,
+            'description' => $this->faker->text,
+        ], $input);
     }
 }
