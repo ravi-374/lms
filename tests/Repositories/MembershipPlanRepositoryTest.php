248095637b97c453a544baf6a876260ea055add9
Ankit Kalathiya <Bhavesh>
2019-08-07 16:39:59 +0530

    test(membership-plan): added test cases of membership plan repository

---
+++ tests/Repositories/MembershipPlanRepositoryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Repositories;
+
+use App\Models\MembershipPlan;
+use App\Repositories\MembershipPlanRepository;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+/**
+ * Class MembershipPlanRepositoryTest.
+ */
+class MembershipPlanRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var MembershipPlanRepository */
+    protected $membershipPlanRepo;
+
+    private $defaultUserId = 1;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->membershipPlanRepo = app(MembershipPlanRepository::class);
+        $this->signInWithDefaultAdminUser();
+    }
+
+    /** @test
+     */
+    public function it_can_store_membership_plan()
+    {
+        $input = [
+            'name'      => $this->faker->name,
+            'price'     => $this->faker->word,
+            'frequency' => MembershipPlan::MONTHLY_FREQUENCY,
+        ];
+
+        $membershipPlanResult = $this->membershipPlanRepo->store($input)->toArray();
+
+        $this->assertArrayHasKey('id', $membershipPlanResult);
+        $this->assertEquals($input['name'], $membershipPlanResult['name']);
+    }
+
+    /** @test */
+    public function it_can_update_membership_plan()
+    {
+        $membershipPlan = factory(MembershipPlan::class)->create();
+        $inputs = [
+            'name'      => $this->faker->name,
+            'price'     => $this->faker->word,
+            'frequency' => MembershipPlan::MONTHLY_FREQUENCY,
+        ];
+
+        $membershipPlan = $this->membershipPlanRepo->update($inputs, $membershipPlan->id)->toArray();
+
+        $this->assertArrayHasKey('id', $membershipPlan);
+        $this->assertEquals($inputs['name'], $membershipPlan['name']);
+    }
+}
