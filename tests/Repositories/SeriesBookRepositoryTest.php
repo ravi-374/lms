b83eeff39c8e316dce8a5c90f3d49a375eb774aa
Ankit Kalathiya <Bhavesh>
2019-08-08 14:46:18 +0530

    test(seriesbook): added test cases of series book repository

---
+++ tests/Repositories/SeriesBookRepositoryTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Tests\Repositories;
+
+use App\Models\Book;
+use App\Models\BookSeries;
+use App\Models\SeriesBook;
+use App\Repositories\SeriesBookRepository;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+/**
+ * Class SeriesBookRepositoryTest.
+ */
+class SeriesBookRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var SeriesBookRepository */
+    protected $seriesBookRepo;
+
+    private $defaultUserId = 1;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seriesBookRepo = app(SeriesBookRepository::class);
+        $this->signInWithDefaultAdminUser();
+    }
+
+    /** @test */
+    public function test_can_create_series_book()
+    {
+        $bookSeries = factory(BookSeries::class)->create([
+            'title' => $this->faker->unique()->word,
+        ]);
+        $book = factory(Book::class)->create([
+            'name' => $this->faker->unique()->name,
+            'isbn' => $this->faker->unique()->isbn10,
+        ]);
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'book_id'   => $book->id,
+            'sequence'  => $this->faker->uuid,
+        ])->toArray();
+
+        $createSeriesBook = $this->seriesBookRepo->createOrUpdateSeriesItems($bookSeries, $inputs);
+
+        $this->assertEquals($bookSeries['book_id'], $createSeriesBook['book_id']);
+    }
+
+    /** @test */
+    public function test_can_not_create_series_book_when_sequence_is_duplicate()
+    {
+        $bookSeries = factory(BookSeries::class)->create([
+            'title' => $this->faker->unique()->word,
+        ]);
+        $book = factory(Book::class)->create([
+            'name' => $this->faker->unique()->name,
+            'isbn' => $this->faker->unique()->isbn10,
+        ]);
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'book_id'   => $book->id,
+            'sequence'  => 1,
+        ])->toArray();
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'book_id'   => $book->id,
+        ])->toArray();
+
+        $response = $this->seriesBookRepo->validateSeriesItems($inputs);
+
+        $this->assertTrue($response, 'Sequence is duplicated');
+    }
+
+    /** @test */
+    public function test_can_not_create_series_book_when_book_is_not_passed()
+    {
+        $bookSeries = factory(BookSeries::class)->create([
+            'title' => $this->faker->unique()->word,
+        ]);
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'sequence'  => 1,
+        ])->toArray();
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'sequence'  => 2,
+        ])->toArray();
+
+        $response = $this->seriesBookRepo->validateSeriesItems($inputs);
+
+        $this->assertTrue($response, 'Book is required');
+    }
+
+    /** @test */
+    public function test_can_not_create_series_book_when_sequence_is_not_passed()
+    {
+        $bookSeries = factory(BookSeries::class)->create([
+            'title' => $this->faker->unique()->word,
+        ]);
+        $book = factory(Book::class)->create([
+            'name' => $this->faker->unique()->name,
+            'isbn' => $this->faker->unique()->isbn10,
+        ]);
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'book_id'   => $book->id,
+            'sequence'  => 1,
+        ])->toArray();
+        $inputs[] = factory(SeriesBook::class)->make([
+            'series_id' => $bookSeries->id,
+            'book_id'   => $book->id,
+        ])->toArray();
+
+        $response = $this->seriesBookRepo->validateSeriesItems($inputs);
+
+        $this->assertTrue($response, 'Sequence is required');
+    }
+}
