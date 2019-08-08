20010814285adabfdd197321459eb751d67b43fc
Ankit Kalathiya <Bhavesh>
2019-08-08 15:47:01 +0530

    test(book-series): added get all book series test case

--- tests/Repositories/BookSeriesRepositoryTest.php
+++ tests/Repositories/BookSeriesRepositoryTest.php
@@ -2,11 +2,8 @@
 
 namespace Tests\Repositories;
 
-use App\Models\Book;
-use App\Models\BookLanguage;
 use App\Models\BookSeries;
-use App\Models\Publisher;
-use App\Repositories\BookRepository;
+use App\Models\SeriesBook;
 use App\Repositories\BookSeriesRepository;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
@@ -30,6 +27,26 @@ class BookSeriesRepositoryTest extends TestCase
         $this->bookSeriesRepo = app(BookSeriesRepository::class);
         $this->signInWithDefaultAdminUser();
     }
+
+    /** @test */
+    public function test_can_get_all_book_series()
+    {
+        /** @var BookSeries $bookSeries */
+        $bookSeries = factory(BookSeries::class)->times(2)->create();
+
+        $seriesBook = factory(SeriesBook::class)->create();
+
+        $bookSeries[0]->seriesItems()->save($seriesBook);
+        $bookSeries[1]->seriesItems()->save($seriesBook);
+
+        $bookSeriesList = $this->bookSeriesRepo->all([]);
+
+        $this->assertEquals($bookSeries[0]->id, $bookSeriesList[1]->id);
+        $this->assertEquals($bookSeries[1]->id, $bookSeriesList[0]->id);
+
+        $this->assertNotEmpty($bookSeriesList[0]->sequence);
+    }
+
 
     /** @test
      */
