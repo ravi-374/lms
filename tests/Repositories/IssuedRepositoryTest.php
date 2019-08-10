7df71aa5258ebd503ccae8979a3f94899cb88a09
Ankit Kalathiya <Bhavesh>
2019-08-10 10:41:11 +0530

    test(issued-book): added test cases of issued book repository

--- tests/Repositories/IssuedRepositoryTest.php
+++ tests/Repositories/IssuedRepositoryTest.php
@@ -38,4 +38,90 @@ class IssuedRepositoryTest extends TestCase
         $this->assertEquals($fakeIssueBook->book_item_id, $issuedBook->bookItem->id);
         $this->assertFalse($issuedBook->bookItem->is_available);
     }
+
+
+    /** @test */
+    public function it_can_store_issued_book()
+    {
+        $bookItem = factory(BookItem::class)->create();
+        $member = factory(Member::class)->create();
+        $input = [
+            'book_item_id' => $bookItem->id,
+            'member_id'    => $member->id,
+        ];
+
+        $issuedBook = $this->issuedBookRepo->store($input)->toArray();
+
+        $this->assertArrayHasKey('id', $issuedBook);
+        $this->assertEquals($input['book_item_id'], $issuedBook['book_item_id']);
+        $this->assertEquals($input['member_id'], $issuedBook['member_id']);
+    }
+
+    /** @test */
+    public function test_can_validate_book()
+    {
+        $book = factory(Book::class)->create();
+        $bookItem = factory(BookItem::class)->create(['book_id' => $book->id]);
+        $member = factory(Member::class)->create();
+        $input = [
+            'book_item_id' => $bookItem->id,
+            'member_id'    => $member->id,
+        ];
+
+        $response = $this->issuedBookRepo->validateBook($input);
+        $this->assertTrue($response, 'Book is not available');
+    }
+
+    /** @test */
+    public function it_can_reserve_book()
+    {
+        /** @var  IssuedBook $fakeReserveBook */
+        $fakeReserveBook = factory(IssuedBook::class)->make(['status' => IssuedBook::STATUS_RETURNED]);
+
+        $issuedBook = $this->issuedBookRepo->reserveBook($fakeReserveBook->toArray());
+
+        $this->assertArrayHasKey('id', $issuedBook);
+        $this->assertEquals(IssuedBook::STATUS_RESERVED, $issuedBook->status);
+
+        $this->assertEquals($fakeReserveBook->book_item_id, $issuedBook->bookItem->id);
+    }
+
+    /** @test */
+    public function it_can_return_book()
+    {
+        $book = factory(Book::class)->create();
+        $bookItem = factory(BookItem::class)->create(['book_id' => $book->id]);
+        /** @var  IssuedBook $fakeIssueBook */
+        $fakeIssueBook = factory(IssuedBook::class)->create([
+            'status'       => IssuedBook::STATUS_RESERVED,
+            'book_item_id' => $bookItem->id,
+        ]);
+
+        $issuedBook = $this->issuedBookRepo->returnBook($fakeIssueBook->toArray());
+
+        $this->assertArrayHasKey('id', $issuedBook);
+        $this->assertEquals(IssuedBook::STATUS_RETURNED, $issuedBook->status);
+
+        $this->assertEquals($fakeIssueBook->book_item_id, $issuedBook->bookItem->id);
+        $this->assertTrue($issuedBook->bookItem->is_available);
+    }
+
+    /** @test */
+    public function it_can_un_reserve_book()
+    {
+        $book = factory(Book::class)->create();
+        $bookItem = factory(BookItem::class)->create(['book_id' => $book->id]);
+        /** @var  IssuedBook $fakeIssueBook */
+        $fakeIssueBook = factory(IssuedBook::class)->create([
+            'status'       => IssuedBook::STATUS_RESERVED,
+            'book_item_id' => $bookItem->id,
+        ]);
+        $issuedBook = $this->issuedBookRepo->unReserveBook($bookItem, $fakeIssueBook->toArray());
+
+        $this->assertArrayHasKey('id', $issuedBook);
+        $this->assertEquals(IssuedBook::STATUS_UN_RESERVED, $issuedBook->status);
+
+        $this->assertEquals($fakeIssueBook->book_item_id, $issuedBook->bookItem->id);
+        $this->assertTrue($issuedBook->bookItem->is_available);
+    }
 }
