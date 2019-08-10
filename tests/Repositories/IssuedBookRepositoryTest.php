04ac66d469851af256a0654aef82446bc5d4871f
Farhan <kathawalafarhan@gmail.com>
2019-08-10 14:29:39 +0530

    test(book): test cases for validate issue book repo

--- tests/Repositories/IssuedBookRepositoryTest.php
+++ tests/Repositories/IssuedBookRepositoryTest.php
@@ -13,7 +13,7 @@ use Tests\TestCase;
 /**
  * Class IssuedRepositoryTest.
  */
-class IssuedRepositoryTest extends TestCase
+class IssuedBookRepositoryTest extends TestCase
 {
     use DatabaseTransactions;
 
@@ -31,9 +31,14 @@ class IssuedRepositoryTest extends TestCase
     public function it_can_issue_book()
     {
         /** @var  IssuedBook $fakeIssueBook */
-        $fakeIssueBook = factory(IssuedBook::class)->make(['status' => IssuedBook::STATUS_AVAILABLE]);
+        $fakeIssueBook = factory(IssuedBook::class)->make();
 
-        $issuedBook = $this->issueBookRepo->issueBook($fakeIssueBook->toArray());
+        $input = [
+            'member_id'    => $fakeIssueBook->member_id,
+            'book_item_id' => $fakeIssueBook->book_item_id,
+        ];
+
+        $issuedBook = $this->issueBookRepo->issueBook($input);
 
         $this->assertArrayHasKey('id', $issuedBook);
         $this->assertEquals(IssuedBook::STATUS_ISSUED, $issuedBook->status);
@@ -42,6 +47,68 @@ class IssuedRepositoryTest extends TestCase
         $this->assertFalse($issuedBook->bookItem->is_available);
     }
 
+    /**
+     * @test
+     * @expectedException Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException
+     * @expectedExceptionMessage Issue date must be less or equal to today's date.
+     */
+    public function test_unable_to_issue_book_when_issue_date_is_greater_than_today_date()
+    {
+        $date = date('Y-m-d h:i:s', strtotime('+2 day'));
+
+        $this->issueBookRepo->issueBook(['issued_on' => $date]);
+    }
+
+    /**
+     * @test
+     * @expectedException Illuminate\Database\Eloquent\ModelNotFoundException
+     * @expectedExceptionMessage No query results for model [App\Models\BookItem] 9999
+     */
+    public function test_unable_to_issue_book_with_non_existing_book_item_id()
+    {
+        $this->issueBookRepo->issueBook(['book_item_id' => 9999]);
+    }
+
+    /**
+     * @test
+     * @expectedException Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException
+     * @expectedExceptionMessage Book is already reserved by another member.
+     */
+    public function test_unable_to_issue_book_when_its_reserved_by_another_member()
+    {
+        /** @var  IssuedBook $issuedBook1 */
+        $issuedBook1 = factory(IssuedBook::class)->create([
+            'status' => IssuedBook::STATUS_RESERVED,
+        ]);
+        $issuedBook2 = factory(IssuedBook::class)->make();
+
+        $input = [
+            'book_item_id' => $issuedBook1->book_item_id,
+            'member_id'    => $issuedBook2->member_id,
+        ];
+
+        $this->issueBookRepo->issueBook($input);
+    }
+
+    /**
+     * @test
+     * @expectedException Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException
+     * @expectedExceptionMessage Book is already issued.
+     */
+    public function test_unable_to_issue_book_when_its_already_issued()
+    {
+        /** @var  IssuedBook $issuedBook */
+        $issuedBook = factory(IssuedBook::class)->create([
+            'status' => IssuedBook::STATUS_ISSUED,
+        ]);
+
+        $input = [
+            'book_item_id' => $issuedBook->book_item_id,
+            'member_id'    => $issuedBook->member_id,
+        ];
+
+        $this->issueBookRepo->issueBook($input);
+    }
 
     /** @test */
     public function it_can_store_issued_book()
