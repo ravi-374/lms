8d8dab26a3a0ef3e0ee30173dc9efba3003a87be
Rupesh Halpati <rupesh.halpati@infyom.com>
2019-08-17 13:36:16 +0530

    fix(book-item): fixed book items

--- resources/lms/src/admin/constants/index.js
+++ resources/lms/src/admin/constants/index.js
@@ -232,3 +232,17 @@ export const bookItemActionType = {
 export const maxDigits = {
     BOOK_CODE: 8
 };
+
+export const bookItemStatusConstants = {
+    AVAILABLE: 1,
+    UNAVAILABLE: 2,
+    LOST: 3,
+    DAMAGE: 4
+};
+
+export const bookItemStatusOptions = [
+    { id: 1, name: 'Available' },
+    { id: 2, name: 'Unavailable' },
+    { id: 3, name: 'Lost' },
+    { id: 4, name: 'Damaged' },
+];
