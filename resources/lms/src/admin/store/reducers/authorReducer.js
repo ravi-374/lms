9733fc6128baa536157f2a9a9c228b1b35ee6405
Rupesh Halpati <rupesh.halpati@infyom.com>
2019-08-21 17:38:16 +0530

    fix(authors): fixed authors data table sorting

--- resources/lms/src/admin/store/reducers/authorReducer.js
+++ resources/lms/src/admin/store/reducers/authorReducer.js
@@ -1,16 +1,15 @@
 import {authorActionType} from '../../constants';
-import _ from 'lodash';
 
-export default (state = {}, action) => {
+export default (state = [], action) => {
     switch (action.type) {
         case authorActionType.FETCH_AUTHORS:
-            return {..._.mapKeys(action.payload, 'id')};
-        case authorActionType.FETCH_AUTHOR:
+            return action.payload;
         case authorActionType.EDIT_AUTHOR:
+            return state.map(item => item.id === +action.payload.id ? action.payload : item);
         case authorActionType.ADD_AUTHOR:
-            return {...state, [action.payload.id]: action.payload};
+            return [...state, action.payload];
         case authorActionType.DELETE_AUTHOR:
-            return _.omit(state, action.payload);
+            return state.filter(item => item.id !== action.payload);
         default:
             return state;
     }
