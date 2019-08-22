47263b379623e6917f62e2a74280d691c2a4bb94
Farhan <kathawalafarhan@gmail.com>
2019-08-22 17:32:24 +0530

    test(book): test cases for issue book API

--- tests/Controllers/IssuedBookAPIControllerTest.php
+++ tests/Controllers/IssuedBookAPIControllerTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Controllers;
 
+use App\Models\Book;
 use App\Models\BookItem;
 use App\Models\IssuedBook;
 use App\Models\Member;
 use App\Repositories\IssuedBookRepository;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Mockery\MockInterface;
 use Tests\TestCase;
@@ -54,6 +56,114 @@ class IssuedBookAPIControllerTest extends TestCase
         $response = $this->getJson('api/b1/books-history');
         $this->assertSuccessMessageResponse($response, 'Issued Books retrieved successfully.');
         $this->assertCount(5, $response->original['data']);
+    }
+
+    /** @test */
+    public function test_can_sort_issued_book_records_by_book_name()
+    {
+        $book1 = factory(Book::class)->create(['name' => 'ABC']);
+        $bookItem1 = factory(BookItem::class)->create(['book_id' => $book1->id]);
+        $issuedBook1 = factory(IssuedBook::class)->create(['book_item_id' => $bookItem1->id]);
+
+        $book2 = factory(Book::class)->create(['name' => 'ZYX']);
+        $bookItem2 = factory(BookItem::class)->create(['book_id' => $book2->id]);
+        $issuedBook2 = factory(IssuedBook::class)->create(['book_item_id' => $bookItem2->id]);
+
+        $responseAsc = $this->getJson('api/b1/books-history?order_by=name&direction=asc');
+        $responseDesc = $this->getJson('api/b1/books-history?order_by=name&direction=desc');
+
+        $responseAsc = $responseAsc->original['data'];
+        $responseDesc = $responseDesc->original['data'];
+        $this->assertEquals($book1->name, $responseAsc[0]['book_item']['book']['name']);
+        $this->assertEquals($book2->name, $responseDesc[0]['book_item']['book']['name']);
+    }
+
+    /** @test */
+    public function test_can_sort_issued_book_records_by_book_code()
+    {
+        $bookItem1 = factory(BookItem::class)->create(['book_code' => 'AB1234XYZ']);
+        $issuedBook1 = factory(IssuedBook::class)->create(['book_item_id' => $bookItem1->id]);
+
+        $bookItem2 = factory(BookItem::class)->create();
+        $issuedBook2 = factory(IssuedBook::class)->create(['book_item_id' => $bookItem2->id]);
+
+        $responseAsc = $this->getJson('api/b1/books-history?order_by=book_code&direction=asc');
+        $responseDesc = $this->getJson('api/b1/books-history?order_by=book_code&direction=desc');
+
+        $responseAsc = $responseAsc->original['data'];
+        $responseDesc = $responseDesc->original['data'];
+        $this->assertEquals($bookItem1->book_code, $responseAsc[0]['book_item']['book_code']);
+        $this->assertEquals($bookItem2->book_code, $responseDesc[0]['book_item']['book_code']);
+    }
+
+    /** @test */
+    public function test_can_sort_issued_book_records_by_member_name()
+    {
+        $member1 = factory(Member::class)->create(['first_name' => 'ABHI']);
+        $issuedBook1 = factory(IssuedBook::class)->create(['member_id' => $member1->id]);
+
+        $member2 = factory(Member::class)->create(['first_name' => 'VISHAL']);
+        $issuedBook2 = factory(IssuedBook::class)->create(['member_id' => $member2->id]);
+
+        $responseAsc = $this->getJson('api/b1/books-history?order_by=member_name&direction=asc');
+        $responseDesc = $this->getJson('api/b1/books-history?order_by=member_name&direction=desc');
+
+        $responseAsc = $responseAsc->original['data'];
+        $responseDesc = $responseDesc->original['data'];
+        $this->assertEquals($member1->first_name, $responseAsc[0]['member']['first_name']);
+        $this->assertEquals($member2->first_name, $responseDesc[0]['member']['first_name']);
+    }
+
+    /** @test */
+    public function test_can_search_issued_book_records_by_status()
+    {
+        $issuedBook1 = factory(IssuedBook::class)->create(['status' => IssuedBook::STATUS_ISSUED]);
+        $issuedBook2 = factory(IssuedBook::class)->create(['status' => IssuedBook::STATUS_RESERVED]);
+
+        $responseIssued = $this->getJson('api/b1/books-history?search=issued&direction=asc');
+        $responseReserved = $this->getJson('api/b1/books-history?search=reserved&direction=desc');
+
+        $responseIssued = $responseIssued->original['data'];
+        $responseReserved = $responseReserved->original['data'];
+        $this->assertEquals(IssuedBook::STATUS_ISSUED, $responseIssued[0]['status']);
+        $this->assertEquals(IssuedBook::STATUS_RESERVED, $responseReserved[0]['status']);
+    }
+
+    /** @test */
+    public function test_can_search_issued_book_records_by_due_date()
+    {
+        $returnDueDate = Carbon::now()->addDays(15)->toDateString();
+        /** @var IssuedBook $issuedBook1 */
+        $issuedBook1 = factory(IssuedBook::class)->create([
+            'status'          => IssuedBook::STATUS_ISSUED,
+            'issued_on'       => Carbon::now(),
+            'return_due_date' => $returnDueDate,
+        ]);
+        $issuedBook2 = factory(IssuedBook::class)->create();
+
+        $response = $this->getJson("api/b1/books-history?due_date=$returnDueDate");
+
+        $response = $response->original['data'];
+        $this->assertCount(1, $response);
+        $this->assertEquals($returnDueDate, $response[0]['return_due_date']);
+    }
+
+    /** @test */
+    public function test_can_search_issued_book_records_by_book_name()
+    {
+        $book1 = factory(Book::class)->create(['name' => 'Laravel']);
+        $bookItem1 = factory(BookItem::class)->create(['book_id' => $book1->id]);
+        $issuedBook1 = factory(IssuedBook::class)->create(['book_item_id' => $bookItem1->id]);
+
+        $book2 = factory(Book::class)->create();
+        $bookItem2 = factory(BookItem::class)->create(['book_id' => $book2->id]);
+        $issuedBook2 = factory(IssuedBook::class)->create(['book_item_id' => $bookItem2->id]);
+
+        $response = $this->getJson("api/b1/books-history?search=$book1->name");
+
+        $response = $response->original['data'];
+        $this->assertCount(1, $response);
+        $this->assertEquals($book1->name, $response[0]['book_item']['book']['name']);
     }
 
     /** @test */
