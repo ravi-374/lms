2be9a988c83320a894777a2029bb7e4f3ac9e295
Rupesh Halpati <rupesh.halpati@infyom.com>
2019-08-28 09:09:23 +0000

    feat(settings):Draft : Settings Enhancement

--- resources/lms/src/admin/components/settings/SettingsForm.js
+++ resources/lms/src/admin/components/settings/SettingsForm.js
@@ -3,10 +3,12 @@ import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form';
 import {Col, Row} from 'reactstrap';
 import Select from '../../../shared/components/Select';
+import InputGroup from '../../../shared/components/InputGroup';
 import settingsFormValidate from '../settings/settingsFormValidate';
 import SaveAction from '../../../shared/action-buttons/SaveAction';
 import {addToast} from '../../../store/action/toastAction';
 import {mapCurrencyCode} from '../../../shared/sharedMethod';
+import {settingsDisplayName} from "../../constants";
 
 const SettingsForm = (props) => {
     const { currencies, initialValues } = props;
@@ -18,23 +20,42 @@ const SettingsForm = (props) => {
     const onSelectCurrency = (option) => {
         setGroupText(mapCurrencyCode(option.id))
     };
-    const onSaveSettings = (formValues) => {
-        const data = {
-            key: 'currency',
-            value: formValues.currencySetting.id,
-            display_name: formValues.currencySetting.name
+
+    const prepareFormData = (key, value, display_name) => {
+        return {
+            key,
+            value,
+            display_name
         };
-        props.onSaveSettings(data);
+    };
+
+    const onSaveSettings = (formValues) => {
+        const { currency, issue_due_days, return_due_days } = formValues;
+        const settings = [prepareFormData('currency', currency.id, currency.name),
+            prepareFormData('issue_due_days', issue_due_days, settingsDisplayName.ISSUE_DUE_DAYS),
+            prepareFormData('return_due_days', return_due_days, settingsDisplayName.RETURN_DUE_DAYS),
+        ];
+        props.onSaveSettings(settings);
     };
     return (
         <Row>
             <Col xs={6}>
-                <Field name='currencySetting' label="Currency" required groupText={groupText} options={currencies}
-                       onChange={onSelectCurrency} component={Select} isSearchable={true} isAuto={true}
+                <Field name='currency' label="Currency" required groupText={groupText} options={currencies}
+                       onChange={onSelectCurrency} placeholder="Select Currency" component={Select} isSearchable={true}
                        innerRef={settingRef}/>
             </Col>
+            <Col xs={6}/>
+            <Col xs={6}>
+                <Field name='issue_due_days' type="number" label="Issue Due Days" min="0" required groupText="calendar"
+                       placeholder="Issue Due Days" component={InputGroup}/>
+            </Col>
+            <Col xs={6}/>
+            <Col xs={6}>
+                <Field name='return_due_days' type="number" label="Return Due Days" min="0" required
+                       groupText="calendar" placeholder="Return Due Days" component={InputGroup}/>
+            </Col>
             <Col xs={12}>
-                <SaveAction onSave={props.handleSubmit(onSaveSettings)} {...props}/>
+                <SaveAction onSave={props.handleSubmit(onSaveSettings)} isHideCancel {...props}/>
             </Col>
         </Row>
     );
--- resources/lms/src/admin/components/settings/settingsFormValidate.js
+++ resources/lms/src/admin/components/settings/settingsFormValidate.js
@@ -1,7 +1,16 @@
 export default formValues => {
     const errors = {};
-    if (!formValues.currencySetting) {
-        errors.currencySetting = 'Currency id must be required.';
+    if (!formValues.currency) {
+        errors.currency = 'Currency must be required.';
+    }
+    if (!formValues.issue_due_days) {
+        errors.issue_due_days = 'Issue due days must be required.';
+    }
+    if (!formValues.reserve_due_days) {
+        errors.reserve_due_days = 'Reserve due days must be required.';
+    }
+    if (!formValues.return_due_days) {
+        errors.return_due_days = 'Return due days must be required.';
     }
     return errors;
 };
