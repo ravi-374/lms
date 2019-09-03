f7923d2d65388d177a12d4fab0c9dde07f61bb04
Farhan <kathawalafarhan@gmail.com>
2019-09-03 17:12:30 +0530

    test(book): test cases for issue book permission

---
+++ tests/B1/APIs/Permissions/IssuedBookPermissionTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Tests\B1\APIs\Permissions;
+
+use App\Models\BookItem;
+use App\Models\IssuedBook;
+use App\Models\Member;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use JWTAuth;
+use Tests\TestCase;
+
+/**
+ * Class IssuedBookPermissionTest
+ * @package Tests\B1\APIs\Permissions
+ */
+class IssuedBookPermissionTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loggedInUserId = factory(User::class)->create();
+        $token = JWTAuth::fromUser($this->loggedInUserId);
+        $this->defaultHeaders = ['HTTP_Authorization' => 'Bearer '.$token];
+    }
+
+    /** @test */
+    public function test_not_allow_to_get_issue_book_without_permission()
+    {
+        $member = factory(Member::class)->create();
+        $bookItem = factory(BookItem::class)->create();
+        $input = ['member_id' => $member->id, 'book_item_id' => $bookItem->id];
+
+        $response = $this->postJson(route('api.b1.issue-book', $bookItem->id), $input);
+
+        $this->assertExceptionMessage($response, 'Unauthorized action.');
+    }
+
+    /** @test */
+    public function test_can_get_issue_book_with_valid_permission()
+    {
+        $this->assignPermissions($this->loggedInUserId, ['issue_books']);
+
+        /** @var Member $member */
+        $member = factory(Member::class)->create();
+
+        /** @var BookItem $bookItem */
+        $bookItem = factory(BookItem::class)->create();
+        $input = ['member_id' => $member->id, 'book_item_id' => $bookItem->id];
+
+        $response = $this->postJson(route('api.b1.issue-book', $bookItem->id), $input);
+
+        $this->assertSuccessMessageResponse($response, 'Book issued successfully.');
+    }
+
+    /** @test */
+    public function test_not_allow_to_get_return_book_without_permission()
+    {
+        /** @var IssuedBook $issueBook */
+        $issueBook = factory(IssuedBook::class)->create();
+
+        $response = $this->postJson(route('api.b1.return-book', $issueBook->book_item_id), [
+            'book_item_id' => $issueBook->book_item_id,
+        ]);
+
+        $this->assertExceptionMessage($response, 'Unauthorized action.');
+    }
+
+    /** @test */
+    public function test_can_get_return_book_with_valid_permission()
+    {
+        $this->assignPermissions($this->loggedInUserId, ['issue_books']);
+
+        /** @var IssuedBook $issueBook */
+        $issueBook = factory(IssuedBook::class)->create();
+
+        $response = $this->postJson(route('api.b1.return-book', $issueBook->book_item_id), [
+            'book_item_id' => $issueBook->book_item_id,
+        ]);
+
+        $this->assertSuccessMessageResponse($response, 'Book return successfully.');
+    }
+
+    /** @test */
+    public function test_not_allow_to_get_book_history_without_permission()
+    {
+        $response = $this->getJson(route('api.b1.books-history'));
+
+        $this->assertExceptionMessage($response, 'Unauthorized action.');
+    }
+
+    /** @test */
+    public function test_can_get_book_history_with_valid_permission()
+    {
+        $this->assignPermissions($this->loggedInUserId, ['issue_books']);
+
+        $response = $this->getJson(route('api.b1.books-history'));
+
+        $this->assertSuccessMessageResponse($response, 'Issued Books retrieved successfully.');
+    }
+
+    /** @test */
+    public function test_not_allow_to_get_issued_book_without_permission()
+    {
+        /** @var IssuedBook $issuedBook */
+        $issuedBook = factory(IssuedBook::class)->create();
+
+        $response = $this->getJson(route('api.b1.issued-book.show', $issuedBook->id));
+
+        $this->assertExceptionMessage($response, 'Unauthorized action.');
+    }
+
+    /** @test */
+    public function test_can_get_issued_book_with_valid_permission()
+    {
+        $this->assignPermissions($this->loggedInUserId, ['issue_books']);
+
+        /** @var IssuedBook $issuedBook */
+        $issuedBook = factory(IssuedBook::class)->create();
+
+        $response = $this->getJson(route('api.b1.issued-book.show', $issuedBook->id));
+
+        $this->assertSuccessMessageResponse($response, 'Issued Book retrieved successfully.');
+    }
+}
---
+++ tests/B1/APIs/Permissions/MemberAPIPermissionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Tests\B1\APIs\Permissions;
+
+
+use App\Models\Member;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use JWTAuth;
+use Tests\TestCase;
+
+/**
+ * Class MemberAPIPermissionTest
+ * @package Tests\B1\APIs\Permissions
+ */
+class MemberAPIPermissionTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loggedInUserId = factory(User::class)->create();
+        $token = JWTAuth::fromUser($this->loggedInUserId);
+        $this->defaultHeaders = ['HTTP_Authorization' => 'Bearer '.$token];
+    }
+
+    /** @test */
+    public function test_can_delete_member_with_valid_permission()
+    {
+        $this->assignPermissions($this->loggedInUserId, ['manage_members']);
+
+        $member = factory(Member::class)->create();
+
+        $response = $this->deleteJson(route('api.b1.members.destroy', $member->id));
+
+        $this->assertExceptionMessage($response, 'Member deleted successfully.');
+    }
+}
