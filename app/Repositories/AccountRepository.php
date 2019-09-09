6fdf6e682604365c813eeb2343855d1b46c41cd7
Farhan <kathawalafarhan@gmail.com>
2019-09-09 11:58:49 +0530

    feat(account): add account module interface

--- app/Repositories/AccountRepository.php
+++ app/Repositories/AccountRepository.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Repositories;
 
+use App\Repositories\Contracts\AccountRepositoryInterface;
 use Exception;
 use Mail;
 use URL;
@@ -9,7 +10,7 @@ use URL;
  * Class AccountRepository
  * @package App\Repositories
  */
-class AccountRepository
+class AccountRepository implements AccountRepositoryInterface
 {
     /**
      * @param  string $username
