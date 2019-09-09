77f9b9521fbf4d6eadfa573ad08cc96986c6a608
Farhan <kathawalafarhan@gmail.com>
2019-09-09 12:14:51 +0530

    feat(book-series): add book series module interface

--- app/Repositories/Contracts/BookSeriesRepositoryInterface.php
+++ app/Repositories/Contracts/BookSeriesRepositoryInterface.php
@@ -1,4 +1,7 @@
 <?php
+use App\Exceptions\ApiOperationFailedException;
+use App\Models\BookSeries;
+use Illuminate\Support\Collection;
 /**
  * Interface BookSeriesRepositoryInterface
  * @package App\Repositories\Contracts
@@ -14,4 +17,33 @@ interface BookSeriesRepositoryInterface
      * @return mixed
      */
     public function model();
+
+    /**
+     * @param array $search
+     * @param int|null $skip
+     * @param int|null $limit
+     * @param array $columns
+     *
+     * @return Collection
+     */
+    public function all($search = [], $skip = null, $limit = null, $columns = ['*']);
+
+    /**
+     * @param array $input
+     *
+     * @throws ApiOperationFailedException
+     *
+     * @return BookSeries
+     */
+    public function store($input);
+
+    /**
+     * @param array $input
+     * @param int $id
+     *
+     * @throws ApiOperationFailedException
+     *
+     * @return BookSeries
+     */
+    public function update($input, $id);
 }
