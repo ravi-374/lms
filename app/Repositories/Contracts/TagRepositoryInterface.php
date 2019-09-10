62f6278ba7ca327cf0040fe05384cb8c3a9e6888
Farhan <kathawalafarhan@gmail.com>
2019-09-10 12:06:56 +0530

    feat(tag): tag interface added

--- app/Repositories/Contracts/TagRepositoryInterface.php
+++ app/Repositories/Contracts/TagRepositoryInterface.php
@@ -3,6 +3,7 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Database\Eloquent\Model;
 /**
  * Interface TagRepositoryInterface
  * @package App\Repositories\Contracts
@@ -18,4 +19,35 @@ interface TagRepositoryInterface
      * @return mixed
      */
     public function model();
+
+    /**
+     * Retrieve all records with given filter criteria
+     *
+     * @param array $search
+     * @param int|null $skip
+     * @param int|null $limit
+     * @param array $columns
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function all($search = [], $skip = null, $limit = null, $columns = ['*']);
+
+    /**
+     * Create model record
+     *
+     * @param array $input
+     *
+     * @return Model
+     */
+    public function create($input);
+
+    /**
+     * Update model record for given id
+     *
+     * @param array $input
+     * @param int $id
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|Model
+     */
+    public function update($input, $id);
 }
