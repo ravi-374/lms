a79c81ac63428c394b8f0944dea24734800cd800
Farhan <kathawalafarhan@gmail.com>
2019-09-10 10:50:28 +0530

    feat(country): add counytry repo interface

---
+++ app/Repositories/Contracts/CountryRepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Repositories\Contracts;
+
+/**
+ * Interface CountryRepositoryInterface
+ * @package App\Repositories\Contracts
+ */
+interface CountryRepositoryInterface
+{
+    /**
+     * @return array
+     */
+    public function getFieldsSearchable();
+
+    /**
+     * @return mixed
+     */
+    public function model();
+
+    /**
+     * Retrieve all records with given filter criteria
+     *
+     * @param array $search
+     * @param int|null $skip
+     * @param int|null $limit
+     * @param array $columns
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function all($search = [], $skip = null, $limit = null, $columns = ['*']);
+}
