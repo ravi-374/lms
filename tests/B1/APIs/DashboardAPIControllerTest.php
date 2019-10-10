9ee5d79e50d773c2d2bcdceca512188354344fa2
vishalinfyom <vishal.ribdiya@infyom.com>
2019-10-10 18:19:37 +0530

    test(dashboard): dashboard api tests added

--- tests/B1/APIs/DashboardAPIControllerTest.php
+++ tests/B1/APIs/DashboardAPIControllerTest.php
@@ -27,14 +27,15 @@ class DashboardAPIControllerTest extends TestCase
     {
         $books = factory(Book::class, 5)->create();
         $members = factory(Member::class, 5)->create();
-        $reservedBooks = factory(IssuedBook::class,
-            10)->create(['status' => IssuedBook::STATUS_RESERVED]); // 10 members/books created here
-        $issuedBooks = factory(IssuedBook::class,
-            2)->create(['status' => IssuedBook::STATUS_ISSUED]); // 2 members/books created here
+        // 10 members/books created here
+        $reservedBooks = factory(IssuedBook::class, 10)->create(['status' => IssuedBook::STATUS_RESERVED]);
+        // 2 members/books created here
+        $issuedBooks = factory(IssuedBook::class, 2)->create(['status' => IssuedBook::STATUS_ISSUED]);
+        // 1 members/books created here
         $overDueBooks = factory(IssuedBook::class)->create([
             'status'          => IssuedBook::STATUS_ISSUED,
             'return_due_date' => Carbon::now()->subDay(),
-        ]); // 1 members/books created here
+        ]);
 
         $response = $this->getJson(route('api.b1.dashboard-details'));
 
@@ -44,6 +45,48 @@ class DashboardAPIControllerTest extends TestCase
         $this->assertEquals(5 + 10 + 2 + 1, $response['total_members']);
         $this->assertEquals(10, $response['total_reserved_books']);
         $this->assertEquals(3, $response['total_issued_books']);
+        $this->assertEquals(1, $response['total_overdue_books']);
+    }
+
+    /** @test */
+    public function test_can_get_dashboard_statistics_for_today()
+    {
+        // books
+        $books = factory(Book::class, 3)->create(['created_at' => Carbon::now()]);
+        factory(Book::class, 2)->create(['created_at' => Carbon::now()->subDays(2)]);
+        // members
+        $members = factory(Member::class, 2)->create(['created_at' => Carbon::now()]);
+        factory(Member::class, 3)->create(['created_at' => Carbon::now()->subDays(2)]);
+        // reserved books
+        $reservedBooks = factory(IssuedBook::class, 4)->create([
+            'status' => IssuedBook::STATUS_RESERVED, 'reserve_date' => Carbon::now(),
+        ]);
+        factory(IssuedBook::class, 1)->create([
+            'status' => IssuedBook::STATUS_RESERVED, 'reserve_date' => Carbon::now()->subDay(),
+        ]);
+        // issue books
+        $issueBooks = factory(IssuedBook::class, 1)->create([
+            'status' => IssuedBook::STATUS_ISSUED, 'issued_on' => Carbon::now(),
+        ]);
+        factory(IssuedBook::class, 4)->create([
+            'status' => IssuedBook::STATUS_ISSUED, 'issued_on' => Carbon::now()->subDay(),
+        ]);
+        // overdue books
+        $overDueBooks = factory(IssuedBook::class, 4)->create([
+            'status' => IssuedBook::STATUS_ISSUED, 'return_due_date' => Carbon::now()->subDay(),
+        ]);
+        factory(IssuedBook::class, 1)->create([
+            'status' => IssuedBook::STATUS_ISSUED, 'return_due_date' => Carbon::now(),
+        ]);
+
+        $response = $this->getJson(route('api.b1.dashboard-details', ['today' => true]));
+
+        $this->assertSuccessMessageResponse($response, 'Dashboard details retrieved successfully.');
+        $response = $response->original['data'];
+        $this->assertEquals(3 + 5 + 5 + 5, $response['total_books']);
+        $this->assertEquals(2 + 5 + 5 + 5, $response['total_members']);
+        $this->assertEquals(4, $response['total_reserved_books']);
+        $this->assertEquals(1, $response['total_issued_books']);
         $this->assertEquals(1, $response['total_overdue_books']);
     }
 }
