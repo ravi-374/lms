f31679e02a0ed5fbeacdacb678752a1d56d33099
Ankit Kalathiya <ankit.infyom@gmail.com>
2020-01-17 13:11:24 +0530

    feat(search-books): added search books api and update member profile api

---
+++ app/Http/Controllers/API/M1/BookItemAPIController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\API\M1;
+
+use App\Http\Controllers\AppBaseController;
+use App\Models\BookItem;
+use App\Repositories\Contracts\BookItemRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+ * Class BookItemAPIController
+ */
+class BookItemAPIController extends AppBaseController
+{
+    /** @var BookItemRepositoryInterface */
+    private $bookItemRepo;
+
+    public function __construct(BookItemRepositoryInterface $bookItemRepo)
+    {
+        $this->bookItemRepo = $bookItemRepo;
+    }
+
+    /**
+     * @param  Request  $request
+     *
+     * @return JsonResponse
+     */
+    public function searchBooks(Request $request)
+    {
+        $input = $request->except(['limit', 'skip']);
+
+        $records = $this->bookItemRepo->searchBooks(
+            $input,
+            $request->get('skip', null),
+            $request->get('limit', null)
+        );
+
+        $records = $records->map(function (BookItem $bookItem) {
+            return $bookItem->apiM1Obj();
+        });
+
+        return $this->sendResponse($records, 'BookItem retrieved successfully.');
+    }
+}
--- routes/api/m1.php
+++ routes/api/m1.php
@@ -1,4 +1,19 @@
 <?php
+
+/**
+ * Member Auth Middleware
+ */
+Route::group(['middleware' => 'member.auth'], function () {
+    // Book search
+    Route::get('search-books', 'BookItemAPIController@searchBooks')->name('search-books.index');
+
+    // update logged in member profile
+    Route::post('update-member-profile', 'MemberAPIController@updateMemberProfile')
+        ->name('update-member-profile');
+
+    Route::put('change-password', 'MemberAPIController@changePassword');
+});
+
 Route::post('member-login', 'AuthAPIController@memberLogin');
 Route::post('register-member', 'MemberAuthController@register')->name('register-member');
 
