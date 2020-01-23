71548031478574987867d52b9043e8531957528c
Vishal Ribdiya <vishal.ribdiya@infyom.com>
2020-01-23 12:02:01 +0530

    refactor: redoc mobile api converted to swagger

--- resources/views/mobile/index.blade.php
+++ resources/views/mobile/index.blade.php
@@ -1,24 +1,17 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>{{ config('app.name') }} APIs</title>
-    <!-- needed for adaptive design -->
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700%7CRoboto:300,400,700" rel="stylesheet">
-
-    <!--
-    ReDoc doesn't change outer page styles
-    -->
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
-    </style>
+    <title>{{ config('app.name') }} | Mobile API's Swagger</title>
+    <link href="{{asset('swagger/style.css')}}" rel="stylesheet">
+</head>
 </head>
 <body>
-<redoc spec-url='{{ asset('mobile/m1/openapi.yaml') }}' hide-download-button></redoc>
-<script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"></script>
+<div id="swagger-ui"></div>
 </body>
+<script src="{{asset('swagger/swagger-bundle.js')}}"></script>
+<script type="application/javascript">
+    const ui = SwaggerUIBundle({
+        url: "{{ asset('mobile/m1/openapi.yaml') }}",
+        dom_id: '#swagger-ui',
+    });
+</script>
 </html>
