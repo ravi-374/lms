1b5d3d809a844f19b1b8ab66a384695332b10fe5
vatsal sakariya <vatsal.sakariya@infyom.com>
2020-03-12 06:36:00 +0000

    password hide show and add filed confirm password

--- resources/lms/src/admin/components/change-password/ChangePasswordForm.js
+++ resources/lms/src/admin/components/change-password/ChangePasswordForm.js
@@ -1,4 +1,4 @@
-import React, {createRef, useEffect} from 'react';
+import React, {useState, createRef, useEffect} from 'react';
 import {Col, Row} from 'reactstrap';
 import {Field, reduxForm} from 'redux-form';
 import PropTypes from 'prop-types';
@@ -9,6 +9,8 @@ import changePasswordValidate from "./changePasswordValidate";
 const ChangePasswordForm = props => {
     const { onSaveChangePassword, handleSubmit } = props;
     const inputRef = createRef();
+    const [isPassword, setIsPassword] = useState(true);
+    const [isConfirmPassword, setIsConfirmPassword] = useState(true);
 
     useEffect(() => {
         inputRef.current.focus();
@@ -18,6 +20,14 @@ const ChangePasswordForm = props => {
         onSaveChangePassword(formValues);
     };
 
+    const onClickShowPassword = () => {
+        setIsPassword(!isPassword);
+    };
+
+    const onClickShowConfirmPassword = () => {
+        setIsConfirmPassword(!isConfirmPassword);
+    };
+
     return (
         <Row className="animated fadeIn m-3">
             <Col xs={12}>
@@ -25,12 +35,14 @@ const ChangePasswordForm = props => {
                        autoComplete={'off'} inputRef={inputRef} groupText="lock" component={InputGroup}/>
             </Col>
             <Col xs={12}>
-                <Field name={'password'} type="password" label="change-password.input.password.label" required
-                       autoComplete={'off'} groupText="lock" component={InputGroup}/>
+                <Field name={'password'} type={isPassword ? "password" : "text"} label="change-password.input.password.label"
+                    required autoComplete={'off'} groupText="lock" component={InputGroup}
+                    appendGroupText={isPassword ? "eye-slash" : "eye"} isAppendIcon onClick={() => onClickShowPassword()}/>
             </Col>
             <Col xs={12}>
-                <Field name={'confirm_password'} type="password" label="change-password.input.confirm-password.label"
-                       autoComplete={'off'} required groupText="lock" component={InputGroup}/>
+                <Field name={'confirm_password'} type={isConfirmPassword ? "password" : "text"} label="change-password.input.confirm-password.label"
+                    autoComplete={'off'} required groupText="lock" component={InputGroup} appendGroupText={isConfirmPassword ? "eye-slash" : "eye"}
+                    isAppendIcon onClick={() => onClickShowConfirmPassword()}/>
             </Col>
             <Col xs={12}>
                 <SaveAction onSave={handleSubmit(onSave)} {...props}/>
