de30d60fa6b5521ec39338951a3dc7affef80e57
Ankit Kalathiya <ankit.infyom@gmail.com>
2020-03-17 14:41:18 +0530

    refactor(penalty): remove extra code from penaltycontroller

--- app/Http/Controllers/API/B1/PenaltyAPIController.php
+++ app/Http/Controllers/API/B1/PenaltyAPIController.php
@@ -3,12 +3,10 @@
 namespace App\Http\Controllers\API\B1;
 
 use App\Http\Controllers\AppBaseController;
-use App\Models\Member;
 use App\Repositories\Contracts\PenaltyRepositoryInterface;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 /**
  * Class PenaltyAPIController
@@ -61,20 +59,13 @@ class PenaltyAPIController extends AppBaseController
     }
 
     /**
-     * @param  Request  $request
      * @param  int  $issuedBookId
      *
      * @throws Exception
      * @return JsonResponse
      */
-    public function sendBookDueMail(Request $request, $issuedBookId)
+    public function sendBookDueMail($issuedBookId)
     {
-        /** @var Member $member */
-        $member = Member::whereEmail($request->get('email'))->first();
-        if (! $member) {
-            throw new UnprocessableEntityHttpException('Given Email does not exist in our system.');
-        }
-
         $this->penaltyRepo->sendBookDueMail($issuedBookId);
 
         return $this->sendSuccess('Your Mail successfully Send.');
