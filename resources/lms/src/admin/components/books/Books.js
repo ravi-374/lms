b178ce50136fb0502f94d5fc46eae978c2f817ae
vatsal sakariya <vatsal.sakariya@infyom.com>
2020-05-26 12:23:16 +0000

    feat: book menu in import or export book and all button in one droup down

--- resources/lms/src/admin/components/books/Books.js
+++ resources/lms/src/admin/components/books/Books.js
@@ -1,24 +1,29 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
 import {Card, CardBody, Col, Row} from 'reactstrap';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import './Books.scss';
-import {publicImagePath, publicImagePathURL} from "../../../appConstant";
+import {publicImagePath} from '../../../appConstant';
 import ProgressBar from '../../../shared/progress-bar/ProgressBar';
 import DeleteBook from './DeleteBook';
-import HeaderTitle from "../../../shared/header-title/HeaderTitle";
-import ModalAction from "../../../shared/action-buttons/ModalAction";
+import HeaderTitle from '../../../shared/header-title/HeaderTitle';
+import ModalAction from '../../../shared/action-buttons/ModalAction';
 import {Routes, icon} from "../../../constants";
-import {getFormattedMessage, prepareFullNames} from "../../../shared/sharedMethod";
-import ReactDataTable from "../../../shared/table/ReactDataTable";
-import {fetchBooks} from '../../store/actions/bookAction';
+import {getFormattedMessage, prepareFullNames} from '../../../shared/sharedMethod';
+import ReactDataTable from '../../../shared/table/ReactDataTable';
+import {fetchBooks, exportBook} from '../../store/actions/bookAction';
 import {toggleModal} from '../../../store/action/modalAction';
-import Viewer from "react-viewer";
+import {toggleImportBookModal} from '../../store/actions/toggleImportBookModal';
+import Viewer from 'react-viewer';
+import ImportBook from './ImportBook';
+import {Dropdown} from 'react-bootstrap';
+import {importBookByFile} from '../../store/actions/fileAction';
 
 const Books = (props) => {
-    const { books, history, isLoading, toggleModal, totalRecord, fetchBooks } = props;
+    const { books, history, isLoading, toggleModal, totalRecord, fetchBooks,
+        toggleImportBookModal, exportBooks, exportBook, importBookByFile } = props;
     const [visible, setVisible] = useState(false);
+    const [importBook, setImportBook] = useState(false);
     const [imageUrl, setImageUrl] = useState('');
     const [book, setBook] = useState(null);
     const cardModalProps = {
@@ -26,8 +31,17 @@ const Books = (props) => {
         toggleModal,
     };
 
+    useEffect(() => {
+        exportBook();
+    }, []);
+
     const onChange = (filter) => {
         fetchBooks(filter, history, true);
+    };
+
+    const onClickModal = () => {
+        setImportBook(true);
+        toggleImportBookModal();
     };
 
     const onOpenModal = (book = null) => {
@@ -100,6 +114,21 @@ const Books = (props) => {
         },
     ];
 
+    const onSaveImportData = async (data) => {
+        const formData = new FormData();
+        formData.append('file', data);
+        importBookByFile(formData, (res) => {
+            if (res.status) {
+                fetchBooks();
+            }
+        });
+        toggleImportBookModal();
+    };
+
+    const importBookModalProps = {
+        onSaveImportData,
+        toggleImportBookModal
+    };
 
     return (
         <Row className="animated fadeIn">
@@ -108,13 +137,29 @@ const Books = (props) => {
                 <HeaderTitle title="Books"/>
                 <h5 className="page-heading">{getFormattedMessage('books.title')}</h5>
                 <div className="d-flex justify-content-end">
-                    <Link to={`${Routes.BOOKS}import-book`} size="md" className="btn btn-primary ml-2">
-                        {getFormattedMessage('books.input.import-btn.label')}
-                    </Link>
-                    <Link to={`${Routes.BOOKS}new`} size="md" className="btn btn-primary ml-2">
-                        {getFormattedMessage('books.input.new-btn.label')}
-                    </Link>
+                    <Dropdown>
+                        <Dropdown.Toggle className="btn btn-primary ml-2" id="dropdown-basic" >
+                            {getFormattedMessage('react-data-table.action.column')}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href={`#${Routes.BOOKS}new`}>
+                                {getFormattedMessage('books.input.new-btn.label')}
+                            </Dropdown.Item>
+                            <Dropdown.Item href={`#${Routes.BOOKS}import-book`}>
+                                {getFormattedMessage('books.input.import-btn.label')}
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={() => onClickModal()}>
+                                {getFormattedMessage('books.import-file-btn.label')}
+                            </Dropdown.Item>
+                            <Dropdown.Item href={exportBooks[0]} download>
+                                {getFormattedMessage('books.export-btn.label')}
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
+                {importBook ?
+                    <ImportBook {...importBookModalProps} />
+                : null}
                 <Viewer drag={false} changeable={false} loop={false} zIndex={1100} scalable={false}
                         noNavbar={true} visible={visible} disableMouseZoom={true} onClose={() => {
                     setVisible(false);
@@ -138,18 +183,19 @@ const Books = (props) => {
     );
 };
 
-
 Books.propTypes = {
     books: PropTypes.array,
     totalRecord: PropTypes.number,
     isLoading: PropTypes.bool,
     fetchBooks: PropTypes.func,
+    exportBook: PropTypes.func,
     toggleModal: PropTypes.func,
+    importBookByFile: PropTypes.func,
 };
 
 const mapStateToProps = (state) => {
-    const { books, isLoading, totalRecord } = state;
-    return { books, isLoading, totalRecord };
+    const { books, isLoading, totalRecord, exportBooks } = state;
+    return { books, isLoading, totalRecord, exportBooks };
 };
 
-export default connect(mapStateToProps, { fetchBooks, toggleModal })(Books);
+export default connect(mapStateToProps, { fetchBooks, exportBook, toggleModal, toggleImportBookModal, importBookByFile })(Books);
