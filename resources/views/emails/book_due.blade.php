3cdc02e51bc7e4a21f6b66e2c8e2b30474d0d151
vishalinfyom <vishal.ribdiya@infyom.com>
2020-05-26 18:24:27 +0530

    refactor: mail template refactored

--- resources/views/emails/book_due.blade.php
+++ resources/views/emails/book_due.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
 
-    Hello {{ $data['first_name']. " ".$data['last_name']}}
+Hello {{ $data['first_name']. " ".$data['last_name']}}
 
-    This is just to remind you that book {{$data['book_name']}}, you have borrowed from the library, was due on {{ $data['due_date'] }} ({{ $data['total_due_days']}} Due Days).
+This is just to remind you that book {{$data['book_name']}}, you have borrowed from the library, was due on {{ $data['due_date'] }} ({{ $data['total_due_days']}} Due Days).
 
-    As per our penalty rule your fine by considering the due days is : {{ $data['total_due_amount']}}
+As per our penalty rule your fine by considering the due days is : {{ $data['total_due_amount']}}
 
-    Please re-submit it to the library.
+Please re-submit it to the library.
 
 
-    Thanks & Regards
-    {{ getSettingValueByKey(\App\Models\Setting::LIBRARY_NAME) }}
+Thanks & Regards
+{{ getSettingValueByKey(\App\Models\Setting::LIBRARY_NAME) }}
 @endcomponent
