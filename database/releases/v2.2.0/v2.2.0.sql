a4747060b0e50a6bee518b427bfa836e3a6d8cdc
vishalinfyom <vishal.ribdiya@infyom.com>
2020-06-05 14:43:47 +0530

    chore: db refreshed

---
+++ database/releases/v2.2.0/v2.2.0.sql
@@ -0,0 +1,2 @@
+INSERT INTO `settings` (`key`, `value`, `display_name`, `created_at`, `updated_at`) VALUES
+('book_due_reminder_before_days', '2', 'Book Due Reminder Before Days', '2020-06-05 03:41:11', '2020-06-05 03:41:11');
